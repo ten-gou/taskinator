879fff645ae7d6d74b029da0bc99440a5318390f
Maribel <lwang79@zoho.com>
2022-02-20 20:33:08 -0800

    updated the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,23 @@
 // connects the button element to the javascript
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 // creates a function to add a new task
 var createTaskHandler = function() {
 
-    // creates the variable li 
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+  // stops the webpage from completing default behaviors- in this case, reloading the page with every form submission
+  event.preventDefault();
 
-    // adds the variable li to the ul
-    tasksToDoEl.appendChild(listItemEl);
-  }
+  // creates the variable li 
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+
+  // adds the variable li to the ul
+  tasksToDoEl.appendChild(listItemEl);
+}
 
   
 // creates the event when the click occurs
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
