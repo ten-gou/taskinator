a991ace3f32048c896fb151262f457621381a645
Maribel <lwang79@zoho.com>
2022-02-21 11:43:14 -0800

    added the function of type to the tasks at hand

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,11 +10,20 @@ var createTaskHandler = function() {
   event.preventDefault();
 
   var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
   // creates the variable li 
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = taskNameInput;
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
 
   // adds the variable li to the ul
   tasksToDoEl.appendChild(listItemEl);
